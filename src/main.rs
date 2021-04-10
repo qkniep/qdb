67c12eae7578334dfdf14c4daee34696aedddf60
Quentin Kniep <hello@quentinkniep.com>
2021-04-10 21:36:06 +0100

    🚧 start working on CLI

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,40 @@ mod relation;
 mod replacer;
 mod table_scan;
 
+use quicli::prelude::*;
+use structopt::StructOpt;
+
 use buffer_manager::BufferManager;
 
-fn main() {
+#[derive(Debug, StructOpt)]
+struct CliArgs {
+    /// How many lines to get
+    #[structopt(long = "count", short = "n", default_value = "3")]
+    count: usize,
+
+    /// The file to read
+    file: String,
+    // Quick and easy logging setup you get for free with quicli
+    #[structopt(flatten)]
+    verbosity: Verbosity,
+}
+
+fn main() -> CliResult {
     let mm = BufferManager::new(1000);
     println!("{}", mm);
+
+    let args = CliArgs::from_args();
+    args.verbosity.setup_env_logger("head")?;
+
+    let content = read_file(&args.file)?;
+    let content_lines = content.lines();
+    let first_n_lines = content_lines.take(args.count);
+
+    info!("Reading first {} lines of {:?}", args.count, args.file);
+
+    for line in first_n_lines {
+        println!("{}", line);
+    }
+
+    Ok(())
 }
